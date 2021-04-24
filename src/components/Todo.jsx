c6bea5c0304b6330fe8171d17b0bbe81f145ee1f
azirm6513 <christianjortiz7+git@gmail.com>
2021-04-24 11:31:30 -0400

    add ability mark off todo item

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import '../App.css';
 
 const Todo = ({ todo, deleteTodo }) => {
+  const completeTodo = (id) => {
+    if (document.getElementById(id).classList.contains('todo-item-complete')) {
+      document.getElementById(id).classList.remove('todo-item-complete');
+      document.getElementById(id + 'btn').removeAttribute('disabled');
+    } else {
+      document.getElementById(id).classList.add('todo-item-complete');
+      document.getElementById(id + 'btn').setAttribute('disabled', true);
+    }
+  };
+
   return (
     <li
+      id={todo.id.toString()}
       className='list-group-item list-group-item-light mt-4'
-      style={{ listStyleType: 'none' }}
+      onClick={() => completeTodo(todo.id)}
     >
       {todo.task}
       <Button
+        id={todo.id.toString() + 'btn'}
         className='ml-5'
         variant='danger'
         size='sm'
